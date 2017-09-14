41db24dfa63ef59b6b27ec7f9307212fa22f4272
Mike Arnold <dharmamike@gmail.com>
2017-09-14 05:38:08 -0500

    Refactor command line handler and Pathfinder module
    Move error handling into the module method and allow for strings to be
    passed to the module method rather than an array

--- lib/pathfinder.rb
+++ lib/pathfinder.rb
@@ -5,6 +5,9 @@ require 'pathfinder/movement'
 
 module Pathfinder
   def self.find_shortest_distance(directions)
+    raise 'Please provide the document with directions' if directions.nil? || directions.empty?
+
+    directions = directions.strip.split(', ')
     start = Position.new(0, 0)
     mapper = Mapper.new(start, directions)
     finish = mapper.destination
--- spec/pathfinder_spec.rb
+++ spec/pathfinder_spec.rb
@@ -5,14 +5,24 @@ RSpec.describe Pathfinder do
     expect(Pathfinder::VERSION).not_to be nil
   end
 
-  it 'calculates the shortest distance' do
-    expect(Pathfinder::find_shortest_distance(%w(R2, L3))).to eq 5
-    expect(Pathfinder::find_shortest_distance(%w(R2, R2, R2))).to eq 2
-    expect(Pathfinder::find_shortest_distance(%w(R5, L5, R5, R3))).to eq 12
-  end
+  describe '.find_shortest_distance' do
+    it 'calculates the shortest distance' do
+      expect(Pathfinder::find_shortest_distance(%q(R2, L3))).to eq 5
+      expect(Pathfinder::find_shortest_distance(%q(R2, R2, R2))).to eq 2
+      expect(Pathfinder::find_shortest_distance(%q(R5, L5, R5, R3))).to eq 12
+    end
 
-  it 'stops the first time a previous position is encountered' do
-    expect(Pathfinder::find_shortest_distance(%w(R8, R4, R4, R8))).to eq 4
-    expect(Pathfinder::find_shortest_distance(%w(R3, R4, R2, R8))).to eq 1
+    it 'stops the first time a previous position is encountered' do
+      expect(Pathfinder::find_shortest_distance(%q(R8, R4, R4, R8))).to eq 4
+      expect(Pathfinder::find_shortest_distance(%q(R3, R4, R2, R8))).to eq 1
+    end
+
+    it 'raises an error when an empty string is passed' do
+      expect{ Pathfinder::find_shortest_distance('') }.to raise_error('Please provide the document with directions')
+    end
+
+    it 'raises an error when no argument is passed' do
+      expect{ Pathfinder::find_shortest_distance(nil) }.to raise_error('Please provide the document with directions')
+    end
   end
 end
