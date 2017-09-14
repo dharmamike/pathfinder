dca052eceea2e0f76be246df295347bc28d61146
Mike Arnold <dharmamike@gmail.com>
2017-09-14 06:48:47 -0500

    Refactor Movement

--- lib/pathfinder/movement.rb
+++ lib/pathfinder/movement.rb
@@ -1,31 +1,30 @@
 module Pathfinder
   class Movement
-    attr_accessor :direction, :distance
-
     def initialize(direction:, distance:)
       @direction = direction
       @distance = distance
     end
 
     def apply(current_position, last_position, previous_positions)
-      calculate_deltas(current_position, last_position)
+      @current_position = current_position
+      @last_position = last_position
+      @previous_positions = previous_positions
 
-      last_position.update(current_position)
-      next_position = step(current_position, @distance)
+      calculate_deltas
+      @last_position.update(@current_position)
+      @next_position = step(@distance)
 
-      until current_position == next_position || previous_positions.add?(current_position).nil? do
-        current_position.update(step(current_position))
-      end
+      @current_position.update(step) until done?
     end
 
-    def calculate_deltas(current_position, last_position)
-      @dx = current_position.xpos - last_position.xpos
-      @dy = current_position.ypos - last_position.ypos
+    def calculate_deltas
+      @dx = @current_position.xpos - @last_position.xpos
+      @dy = @current_position.ypos - @last_position.ypos
     end
 
-    def step(current_position, blocks = 1)
-      xpos = current_position.xpos
-      ypos = current_position.ypos
+    def step(blocks = 1)
+      xpos = @current_position.xpos
+      ypos = @current_position.ypos
 
       if @dy > 0
         xpos = @direction == 'R' ? xpos + blocks : xpos - blocks
@@ -40,6 +39,14 @@ module Pathfinder
       end
 
       Position.new(xpos, ypos)
+    end
+
+    def done?
+      @current_position == @next_position || intersected?
+    end
+
+    def intersected?
+      @previous_positions.add?(@current_position).nil?
     end
   end
 end
